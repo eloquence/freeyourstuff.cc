9a9c348c05850beb1f3ad7a73bde54eb2d45bab1
Erik Moeller <eloquence@gmail.com>
2016-03-30 18:18:02 -0700

    Use site metadata to find registered plugins/schemas.
    This simplifies loading schemas, and eliminates some dependencies.
    We also stash site metadata in the schema object, since it's useful
    for tests.

--- service/load-schemas.js
+++ service/load-schemas.js
@@ -1,33 +1,27 @@
 // This module loads and exposes an object of all available schemas, using
-// the schema key as the object key.
+// the schema key as the object key. It adds the site metadata to the schema
+// header for convenient access.
 //
-// The schemas are loaded from the extension plugin directory. We therefore need
-// to have a checkout of the extension alongside the web service in order to use
-// this module.
+// The schemas and site metadata are loaded from the extension plugin directory.
+// We therefore need to have a checkout of the extension alongside the web
+// service in order to use this module.
 'use strict';
 let jsonfile = require('jsonfile');
-let fs = require('fs');
-let path = require('path');
+let sitesDir = '../extension';
 let pluginDir = '../extension/src/plugins';
-let dirs = getDirectories(pluginDir);
+let sites = jsonfile.readFileSync(`${sitesDir}/sites.json`);
 let schemas = {};
-for (let dir of dirs) {
-  try {
-    let schema = jsonfile.readFileSync(`${pluginDir}/${dir}/schema.json`);
-    if (schema.hasOwnProperty('schema') && schema.schema.hasOwnProperty('key')) {
-      schemas[schema.schema.key] = schema;
-    } else {
-      console.log(`schema.json for plugin "${dir}" does not have a valid schema header. Skipping.`);
+for (let site of sites) {
+    try {
+      let schema = jsonfile.readFileSync(`${pluginDir}/${site.plugin}/schema.json`);
+      if (schema.hasOwnProperty('schema') && schema.schema.hasOwnProperty('key')) {
+        schemas[schema.schema.key] = schema;
+        schema.schema.site = site;
+      } else {
+        console.log(`schema.json for plugin "${site.plugin}" does not have a valid schema header. Skipping.`);
+      }
+    } catch (e) {
+      console.log(`Problem reading schema.json for plugin "${site.plugin}". Skipping. Error was: ${e}`);
     }
-  } catch (e) {
-    console.log(`Problem reading schema.json for plugin "${dir}". Skipping.`);
-  }
 }
-
-function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
-}
-
 module.exports = schemas;

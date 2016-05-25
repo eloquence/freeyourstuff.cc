429e9d2e351a4fdd3a48b931eb08cd6671e5b829
Erik Moeller <eloquence@gmail.com>
2016-05-24 19:58:35 -0700

    formatting

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,23 +1,22 @@
 (function() {
   'use strict';
-  chrome.runtime.onMessageExternal.addListener(
-    (request, sender, sendResponse) => {
-      if (request) {
-        if (request.message && request.message == "version") {
-          sendResponse({
-            version: chrome.runtime.getManifest().version
-          });
-        }
+  chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+    if (request) {
+      if (request.message && request.message == "version") {
+        sendResponse({
+          version: chrome.runtime.getManifest().version
+        });
       }
-      return true;
-    });
+    }
+    return true;
+  });
 
   chrome.runtime.onMessage.addListener((request, sender) => {
     if (request.action == 'dispatch' && request.data && request.schema) {
       chrome.tabs.create({
         url: 'src/result.html',
         active: true
-      }, (tab) => {
+      }, tab => {
         function listener(newTab, newChange, newTabObj) {
           if (newTab === tab.id && newTabObj.status === 'complete') {
             chrome.runtime.sendMessage({
